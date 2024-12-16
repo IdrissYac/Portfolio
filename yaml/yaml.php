5ac9cbb9838738a066826dd9921e73dac0284081
Idriss Yacoubi <iyacoubi@info.local>
2024-12-16 11:08:35 +0100

    modification fichier yaml.php

--- yaml/yaml.php
+++ yaml/yaml.php
@@ -11,7 +11,7 @@ use Symfony\Component\Yaml\Exception\ParseException;
 if (!function_exists('yaml_parse_file')) {
 	function yaml_parse_file($file) {
 		try {
-			return Yaml: :parseFile($file);
+			return Yaml::parseFile($file);
 		} catch (ParseException $exception) {
 			printf('Unable to parse the YAML string: <b>%s</b>'. $exception->)
 		}
